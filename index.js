223f697cc91b7252e277dcdc7973bc98b81bb5da
aprzygocki <augustino.przygocki@yahoo.com>
2020-10-04 22:11:39 -0700

    Initialized API response

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-console */
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
+app.use(routes);
 app.listen(3001, () => {
   console.log('Server started listening on PORT http://localhost:3001');
 });
---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+// api prepended to every Route
+router.get('/', (_req, res) => {
+  res.send('Hello');
+})
+module.exports = router;
